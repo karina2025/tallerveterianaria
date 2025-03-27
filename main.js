ff8638b382407507b43221e8b04d560afc77170e
karina casas <casaskarina817@gmail.com>
2025-03-27 12:49:31 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+let mascotas =JSON.parse(localStorage.getItem("mascotas")) || [];
+
+
+function mascotanueva(callback) {
+    setTimeout(()=> {
+        let nuevamascota = {
+            nombre:prompt
+        }
+
+    }
+    )
+}
+
+
+
+
+
